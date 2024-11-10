2152955a757e74b43705a1688f0029c7abd471b4
Coders-Anas <anasashrafansari896@gmail.com>
2024-11-10 23:48:47 +0500

    Update page.tsx

--- portfolio-tailwind/src/app/projects/page.tsx
+++ portfolio-tailwind/src/app/projects/page.tsx
@@ -10,7 +10,10 @@ export default function page() {
         <div id="projects" className="grid gap-4 grid-cols-1 lg:grid-cols-2">
           <div className="card bg-base-100 w-96 shadow-xl">
             <div className="card-body">
-              <h2 className="card-title">Static Resume</h2>
+              <h2 className="card-title"><a href="https://milestones-1-2-pink.vercel.app/"></a>Static Resume</h2>
+              <h2 className="card-title"><a href="https://milestone-3-taupe.vercel.app/"></a>Dynamic Resume</h2>
+              <h2 className="card-title"><a href="https://milestone-4-peach.vercel.app/"></a>Editable Resume</h2>
+              <h2 className="card-title"><a href="https://milestones-5-puce.vercel.app/"></a>Shareable Resume</h2>
               <p>
                 A static resume built with HTML, CSS, JavaScript, and
                 TailwindCSS. It showcases my personal information in a user

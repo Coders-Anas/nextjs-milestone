19ec30c86ce18bb837d2e8f9aee3c3d693fe693f
Coders-Anas <anasashrafansari896@gmail.com>
2024-11-12 22:56:59 +0500

    fixed bug

--- portfolio-custom/src/app/projects/page.tsx
+++ portfolio-custom/src/app/projects/page.tsx
@@ -17,8 +17,31 @@ export default function page() {
               </p>
               <div className="project-card__actions">
                 <a href="https://hackathon-milestone-liard.vercel.app/" className="btn btn-accent">Visit</a>
-                <a href="" className="btn btn-accent">Visit</a>
+              </div>
+            </div>
+          </div>
+
+          <div className="card bg-base-100 w-96 shadow-xl">
+            <div className="card-body">
+              <h2 className="card-title">Static Resume</h2>
+              <p>
+                A static resume built with HTML, CSS, JavaScript, and
+                TailwindCSS. It showcases my personal information in a user-friendly and beautifully designed UI.
+              </p>
+              <div className="project-card__actions">
                 <a href="https://hackathon-milestone-gfd3.vercel.app/" className="btn btn-accent">Visit</a>
+              </div>
+            </div>
+          </div>
+
+          <div className="card bg-base-100 w-96 shadow-xl">
+            <div className="card-body">
+              <h2 className="card-title">Static Resume</h2>
+              <p>
+                A static resume built with HTML, CSS, JavaScript, and
+                TailwindCSS. It showcases my personal information in a user-friendly and beautifully designed UI.
+              </p>
+              <div className="project-card__actions">
                 <a href="https://hackathon-milestone-f2xo.vercel.app/" className="btn btn-accent">Visit</a>
               </div>
             </div>
